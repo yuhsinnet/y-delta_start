d5d4cba1941abbe34d416a78d504b4d88f4459a3
x78701542000 <x78701542000@livemail.tw>
2019-07-28 08:53:48 +0800

    新增中斷功能

--- main.c
+++ main.c
@@ -15,16 +15,38 @@ int main(void) {
     /* Replace with your application code */
 
     OUTPUTp(DDRB, 0);
+    //Setup Timer  :CTC mode ;1ms cycle.ATtiny13A
+#define OCRX ((F_CPU-512000)/512000)*2
+    TCCR0A |= (1 << WGM01);
+    TCCR0B |= (1 << CS02);
+    OCR0A = OCRX;
+    TIMSK0 |= 1 << OCIE0A;
+    TIFR0 |= 1 << OCF0A;
+    //
 
+    sei();
     while (1) {
-        if (READp(PINB, 3) == HIGH) {
-            SETp(PORTB, 0);
-            _delay_ms(500);
-            CLEARp(PORTB, 0);
-            _delay_ms(500);
-        }
+        //        if (READp(PINB, 3) == HIGH) {
+        //            SETp(PORTB, 0);
+        //            _delay_ms(500);
+        //            CLEARp(PORTB, 0);
+        //            _delay_ms(500);
+        //        }
 
 
 
     }
+}
+
+ISR(TIM0_COMPA_vect) {
+
+    static uint16_t i;
+    if (i++ > 30) {
+        TOGGLEp(PORTB, 0);
+
+        i = 0;
+    }
+
+
+
 }
